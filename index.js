12bdefafd8795c41b1abd3f95be9ddecfa629ff5
jikisoria <soriasamsung@gmail.com>
2023-01-10 01:39:08 +0800

    additionals lol

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ window.addEventListener("click", function playMusic() {
     }
 
 })
+
+
+window.addEventListener("load", function(){
+
+    
+    var h2 = document.querySelectorAll("h2").length;
+
+    for(var i = 0; i<h2; i++){
+        document.querySelectorAll("h2")[i].classList.add("animate__animated",  "animate__jackInTheBox",  "animate__infinite", "animate__slower");
+    }
+
+
+})
